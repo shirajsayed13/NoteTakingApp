63987269afcb6cf58c904d33bebffcfe8522f28b
Shiraj Sayed <shirajsayed13@gmail.com>
2020-01-24 21:44:57 +0530

    add test cases for DateUtil, TimeStamp

--- app/src/test/java/com/shiraj/notetakingapp/util/DateUtilTest.java
+++ app/src/test/java/com/shiraj/notetakingapp/util/DateUtilTest.java
@@ -1,8 +1,18 @@
 package com.shiraj.notetakingapp.util;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Random;
+
+import static com.shiraj.notetakingapp.util.DateUtil.GET_MONTH_ERROR;
+import static com.shiraj.notetakingapp.util.DateUtil.getMonthFromNumber;
+import static com.shiraj.notetakingapp.util.DateUtil.monthNumbers;
+import static com.shiraj.notetakingapp.util.DateUtil.months;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DateUtilTest {
@@ -17,5 +27,20 @@ public class DateUtilTest {
                 System.out.println("Timestamp is generated correctly");
             }
         });
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11})
+    public void getMonthFromNumber_returnSuccess(int monthNumber, TestInfo testInfo, TestReporter testReporter) {
+        assertEquals(months[monthNumber], DateUtil.getMonthFromNumber(monthNumbers[monthNumber]));
+        System.out.println(monthNumbers[monthNumber] + " : " + months[monthNumber]);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11})
+    public void getMonthFromNumber_returnError(int monthNumber, TestInfo testInfo, TestReporter testReporter) {
+        int randomInt = new Random().nextInt(90) + 10;
+        assertEquals(getMonthFromNumber(String.valueOf(monthNumber * randomInt)), GET_MONTH_ERROR);
+        System.out.println(monthNumbers[monthNumber] + " : " + GET_MONTH_ERROR);
     }
 }
